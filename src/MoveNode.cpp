1b965ad58f60172487f3dc56ac6c06c5f4279cba
AlexandruMihai18 <alexandrumihai200913@gmail.com>
2023-05-22 19:03:58 +0300

    Add structure for evaluate functions
    Signed-off-by: AlexandruMihai18 <alexandrumihai200913@gmail.com>

--- src/MoveNode.cpp
+++ src/MoveNode.cpp
@@ -4,15 +4,6 @@
 
 extern std::ofstream fout;
 
-BoardConfig::BoardConfig(
-    PlaySidePiece currentConfig[TABLE_SIZE + 1][TABLE_SIZE + 1]) {
-    for (int i = 1; i <= TABLE_SIZE; i++) {
-        for (int j = 1; j <= TABLE_SIZE; j++) {
-            this->config[i][j] = currentConfig[i][j];
-        }
-    }
-}
-
 MoveNode::MoveNode(MoveNode *moveNode, Move *move) {
 	for (int i = 1; i <= TABLE_SIZE; i++) {
         for (int j = 1; j <= TABLE_SIZE; j++) {
--- src/MoveNode.h
+++ src/MoveNode.h
@@ -5,35 +5,10 @@
 
 #include "Move.h"
 #include "PlaySide.h"
+#include "BoardHelpers.h"
 
-#define TABLE_SIZE 8
 #define KNIGHT_MOVES 8
 #define KING_MOVES 9
-
-enum PlaySidePiece {
-  WHITE_PAWN = 1, WHITE_ROOK = 2,
-  WHITE_KNIGHT = 3, WHITE_BISHOP = 4,
-  WHITE_QUEEN = 5, WHITE_KING = 6,
-  WHITE_EN_PAS = 11, P_WHITE_ROOK = 12,
-  P_WHITE_KNIGHT = 13, P_WHITE_BISHOP = 14, 
-  P_WHITE_QUEEN = 15,
-  BLACK_PAWN = -1, BLACK_ROOK = -2,
-  BLACK_KNIGHT = -3, BLACK_BISHOP = -4,
-  BLACK_QUEEN = -5, BLACK_KING = -6,
-  BLACK_EN_PAS = -11, P_BLACK_ROOK = -12,
-  P_BLACK_KNIGHT = -13, P_BLACK_BISHOP = -14,
-  P_BLACK_QUEEN = -15,
-  NO_PIECE = 0
-};
-
-class BoardConfig {
-	public:
-
-	PlaySidePiece config[TABLE_SIZE + 1][TABLE_SIZE + 1];
-
-	/* copy constructor for board configuration */
-	BoardConfig(PlaySidePiece currentConfig[TABLE_SIZE + 1][TABLE_SIZE + 1]);
-};
 
 class MoveNode {
 	public:
