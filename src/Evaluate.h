a605a2c8b5d5e006f4505954c05ed4c8dffc6788
AlexandruMihai18 <alexandrumihai200913@gmail.com>
2023-05-22 19:20:52 +0300

    Add scores for each piece and moves
    Signed-off-by: AlexandruMihai18 <alexandrumihai200913@gmail.com>

--- src/Evaluate.h
+++ src/Evaluate.h
@@ -3,9 +3,22 @@
 
 #include "BoardHelpers.h"
 
-bool isWhite(PlaySidePiece piece);
+#define DOUBLE_PAWNS_S -0.5
+#define ISOLATED_PAWNS_S -0.5
+#define KNIGHT_EARLY_S 1.5
+#define ATTACK_NEAR_KING_S 1.5
+#define MOBILITY_S 0.05
 
-double evaluate_basic(BoardConfig config, bool canCastle, int side);
+enum PieceScore {
+  PAWN_S = 1, ROOK_S = 5,
+  KNIGHT_S = 3, BISHOP_S = 3,
+  QUEEN_S = 9, KING_S = 1000,
+  P_ROOK_S = 0, P_KNIGHT_S = 6,
+  P_BISHOP_S = 0, P_QUEEN_S = 17,
+  CASTLE_S = 3, CENTER_PAWN = 2,
+};
+
+double evaluate_basic(BoardConfig config, bool canCastle, int possible_moves, int side);
 
 double evaluate_early(BoardConfig config, bool canCastle, int side);
 
